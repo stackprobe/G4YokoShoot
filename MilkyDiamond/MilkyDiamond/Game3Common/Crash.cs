40f24323ab18413b932929fec57063e3719fd0eb
stackprobe <stackprobes@gmail.com>
2019-09-13 08:57:04 +0900

    Rose

---
+++ MilkyDiamond/MilkyDiamond/Game3Common/Crash.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Tools;
+
+namespace Charlotte.Game3Common
+{
+	//
+	//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+	//
+	public class Crash
+	{
+		// インスタンスの生成は CrashUtils で、
+
+		//
+		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+		//
+		public enum Kind_e
+		{
+			NONE = 1,
+			POINT,
+			CIRCLE,
+			RECT,
+		}
+
+		//
+		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+		//
+		public Kind_e Kind = Kind_e.POINT;
+		//
+		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+		//
+		public D2Point Pt = new D2Point(-1, 0);
+		//
+		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+		//
+		public double R = 0.0;
+		//
+		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+		//
+		public D4Rect Rect = null;
+
+		//
+		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+		//
+		public bool IsCrashed(Crash other)
+		{
+			return CrashUtils.IsCrashed(this, other);
+		}
+	}
+}
---
+++ MilkyDiamond/MilkyDiamond/Game3Common/CrashUtils.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Tools;
+using Charlotte.Common;
+
+namespace Charlotte.Game3Common
+{
+	//
+	//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+	//
+	public static class CrashUtils
+	{
+		//
+		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+		//
+		public static Crash None()
+		{
+			return new Crash();
+		}
+
+		//
+		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+		//
+		public static Crash Point(D2Point pt)
+		{
+			return new Crash()
+			{
+				Kind = Crash.Kind_e.POINT,
+				Pt = pt,
+			};
+		}
+
+		//
+		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+		//
+		public static Crash Circle(D2Point pt, double r)
+		{
+			return new Crash()
+			{
+				Kind = Crash.Kind_e.CIRCLE,
+				Pt = pt,
+				R = r,
+			};
+		}
+
+		//
+		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+		//
+		public static Crash Rect_CenterSize(D2Point centerPt, D2Size size)
+		{
+			return Rect(new D4Rect(centerPt.X - size.W / 2.0, centerPt.Y - size.H / 2.0, size.W, size.H));
+		}
+
+		//
+		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+		//
+		public static Crash Rect(D4Rect rect)
+		{
+			return new Crash()
+			{
+				Kind = Crash.Kind_e.RECT,
+				Rect = rect,
+			};
+		}
+
+		//
+		//	copied the source file by https://github.com/stackprobe/Factory/blob/master/SubTools/CopyLib.c
+		//
+		public static bool IsCrashed(Crash a, Crash b)
+		{
+			if ((int)b.Kind < (int)a.Kind)
+			{
+				Crash tmp = a;
+				a = b;
+				b = tmp;
+			}
+			if (a.Kind == Crash.Kind_e.NONE)
+				return false;
+
+			if (a.Kind == Crash.Kind_e.POINT)
+			{
+				if (b.Kind == Crash.Kind_e.POINT)
+					return false;
+
+				if (b.Kind == Crash.Kind_e.CIRCLE)
+					return DDUtils.IsCrashed_Circle_Point(b.Pt, b.R, a.Pt);
+
+				if (b.Kind == Crash.Kind_e.RECT)
+					return DDUtils.IsCrashed_Rect_Point(b.Rect, a.Pt);
+
+				throw new DDError();
+			}
+			if (a.Kind == Crash.Kind_e.CIRCLE)
+			{
+				if (b.Kind == Crash.Kind_e.CIRCLE)
+					return DDUtils.IsCrashed_Circle_Circle(a.Pt, a.R, b.Pt, b.R);
+
+				if (b.Kind == Crash.Kind_e.RECT)
+					return DDUtils.IsCrashed_Circle_Rect(a.Pt, a.R, b.Rect);
+
+				throw new DDError();
+			}
+			if (a.Kind == Crash.Kind_e.RECT)
+			{
+				if (b.Kind == Crash.Kind_e.RECT)
+					return DDUtils.IsCrashed_Rect_Rect(a.Rect, b.Rect);
+
+				throw new DDError();
+			}
+			throw new DDError();
+		}
+	}
+}
