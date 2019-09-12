5473163ee0bcefa62b7bb51100a32ae8ba8d1fb3
stackprobe <stackprobes@gmail.com>
2019-09-12 15:12:00 +0900

    Macaron

--- _Release.bat
+++ _Release.bat
@@ -12,7 +12,7 @@ COPY /B MilkyDiamond\MilkyDiamond\bin\Release\DxLibDotNet.dll out
 C:\Factory\Tools\xcp.exe doc out
 C:\Factory\Tools\xcp.exe C:\Dev\Fairy\Donut2\doc out
 
-C:\Factory\SubTools\zip.exe /PE- /G out MilkyDiamond
+C:\Factory\SubTools\zip.exe /PE- /RVE- /G out MilkyDiamond
 C:\Factory\Tools\summd5.exe /M out
 
 PAUSE
