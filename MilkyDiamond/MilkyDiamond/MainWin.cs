f2ac8a759b5261e89d105ebed4d301843eed3755
stackprobe <stackprobes@gmail.com>
2019-11-07 03:18:38 +0900

    Sword

--- MilkyDiamond/MilkyDiamond/MainWin.cs
+++ MilkyDiamond/MilkyDiamond/MainWin.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
+using System.Security.Permissions;
 using Charlotte.Tools;
 using Charlotte.Common;
 
@@ -17,6 +18,22 @@ namespace Charlotte
 	public partial class MainWin : Form
 	{
 		// sync > @ G2_MainWin
+
+		#region ALT_F4 抑止
+
+		[SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+		protected override void WndProc(ref Message m)
+		{
+			const int WM_SYSCOMMAND = 0x112;
+			const long SC_CLOSE = 0xF060L;
+
+			if (m.Msg == WM_SYSCOMMAND && (m.WParam.ToInt64() & 0xFFF0L) == SC_CLOSE)
+				return;
+
+			base.WndProc(ref m);
+		}
+
+		#endregion
 
 		public MainWin()
 		{
